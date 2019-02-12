76ef6d5e90803d928a4ea4e604e97ccad159e0e7
MrDanielBennett <daniel.bennett1999@gmail.com>
2019-02-12 14:46:08 -0800

    add more to webpack and styles as well as a loop using map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,13 @@ import TicketList from './TicketList';
 function App(){
   return (
     <div>
+      <style jsx global>{`
+          body {
+            background-color: #303030;
+            font-family: Helvetica;
+            color: white;
+          }
+            `}</style>
       <Header/>
       <TicketList/>
     </div>
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types';
 
 function Ticket({names, location, issue}){
   let styledTickets = {
-    backgroundColor: '#212121',
+    backgroundColor: '#303030',
     fontFamily: 'sans-serif',
     color: '#ffffff',
     paddingTop: '30'
-  }
+  };
   return(
-    <div style={styledTickets}>
+    <div>
+      <style global jsx>{`
+          div {
+            background-color: #424242;
+          }
+              `}</style>
       <h3>{location} - {names}</h3>
       <p><em>{issue}</em></p>
       <hr/>
